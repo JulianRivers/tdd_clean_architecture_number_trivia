bba01d4c64c7d0d2926293deade6947d1dd59cd3
JulianRivers <juliancamilorifo@ufps.edu.co>
2025-07-06 11:42:45 -0500

    task: pokemon card

---
+++ lib/features/pokemon/presentation/widgets/pokemon_card.dart
@@ -0,0 +1,20 @@
+import 'package:clean_architecture_poke_app/features/pokemon/domain/entities/pokemon.dart';
+import 'package:flutter/material.dart';
+
+class PokemonCard extends StatelessWidget {
+  final Pokemon pokemon;
+
+  const PokemonCard({super.key, required this.pokemon});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Column(
+        children: [
+          Image.network(pokemon.image),
+          Text(pokemon.name)
+        ],
+      ),
+    );
+  }
+}
